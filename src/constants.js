56831db92d88958e8f370c0e75562163fd43e455
Ayushya <jaiswalayushya@gmail.com>
2025-05-18 16:25:35 +0530

    Initialized service with basic configurations required

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const SUBMISSION_API = '/submission-svc/api/v1.0';
+
+const serviceConfig = {
+  serviceName: 'submission-svc',
+  HOST: 'localhost',
+  PORT: 5400,
+  PROTOCOL: 'http',
+};
+
+export { SUBMISSION_API, serviceConfig };
